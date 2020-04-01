720f5ef597fa79a54df25cb795162875aa2b8b1d
Julio E. Martinez Thorpe <julio@wakafoo.com>
2020-04-01 16:46:15 -0700

    Some updates to include numeric keypad and F keys.

--- keyboards/preonic/keymaps/j3m7/keymap.c
+++ keyboards/preonic/keymaps/j3m7/keymap.c
@@ -311,18 +311,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = LAYOUT_preonic_grid( \
-  KC_TILD, _______, _______, LF_DIE,  _______, _______, _______, _______, TD_PARN, KC_RPRN, KC_VOLD, KC_VOLU, \
-  _______, LF_DIE,  _______, _______, _______, _______, _______, _______, TD_BRAC, KC_RBRC, KC_MPLY, KC_DEL,  \
-  KC_RCTL, _______, _______, _______, _______, _______, _______, _______, TD_CBRC, KC_RCBR, _______, KC_BSLS, \
-  KC_RSFT, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_PGUP, LF_DOIT, \
-  KC_ESC,  _______, KC_RALT, KC_RGUI, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END   \
+  KC_TILD, _______, _______, LF_DIE,  _______, KC_NLCK, KC_PSLS, KC_PAST, TD_PARN, KC_RPRN, KC_VOLD, KC_VOLU, \
+  _______, LF_DIE,  _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   TD_BRAC, KC_RBRC, KC_MPLY, KC_DEL,  \
+  KC_RCTL, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   TD_CBRC, KC_RCBR, _______, KC_BSLS, \
+  KC_RSFT, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_MPRV, KC_MNXT, KC_PGUP, LF_DOIT, \
+  KC_ESC,  _______, KC_RALT, KC_RGUI, _______, KC_P0,   KC_P0,   _______, _______, KC_HOME, KC_PGDN, KC_END   \
 ),
 
 [_RAISE] = LAYOUT_preonic_grid(						\
-  KC_GRV,  _______, _______, LF_DIE,  _______, _______, _______, _______, TD_PARN, KC_RPRN, KC_VOLD, KC_VOLU, \
-  _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______, _______, TD_BRAC, KC_RBRC, KC_MPLY, KC_DEL, \
-  KC_RCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, _______, TD_CBRC, KC_RCBR, _______, KC_PIPE, \
-  KC_RSFT, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_PGUP, LF_DOIT, \
+  KC_GRV,  _______, _______, LF_DIE,  _______, KC_F1,   KC_F2,   KC_F3,   TD_PARN, KC_RPRN, KC_VOLD, KC_VOLU, \
+  _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_F4,   KC_F5,   KC_F6,   TD_BRAC, KC_RBRC, KC_MPLY, KC_DEL, \
+  KC_RCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_F7,   KC_F8,   KC_F9,   TD_CBRC, KC_RCBR, _______, KC_PIPE, \
+  KC_RSFT, _______, _______, _______, _______, KC_F10,  KC_F11,  KC_F12,  KC_MPRV, KC_MNXT, KC_PGUP, LF_DOIT, \
   KC_ESC,  KC_RCTL, KC_RALT, KC_RGUI, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
 ),
 
@@ -330,7 +330,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, _______, \
   KC_CAPS, EEP_RST, MU_MOD,  AU_ON,   AU_OFF,  _______, MC_HELP, MC_FREK, _______, _______,  _______, _______, \
-  KC_INS, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
+  KC_INS,  MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
 };
