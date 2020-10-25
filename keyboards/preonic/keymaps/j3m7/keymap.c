363c21174b6dfff010e67589142deae6ed61f5a4
Julio E. Martinez Thorpe <julio@wakafoo.com>
2020-10-25 13:29:10 -0700

    Reduce and simplify.
    Removed encoder,
    Removed unused layouts
    Disabled terminal, bootmagic
    Removed LF_HOME
    Addded leader key setup
    Documented in quick help LEADER+H

--- keyboards/preonic/keymaps/j3m7/keymap.c
+++ keyboards/preonic/keymaps/j3m7/keymap.c
@@ -19,10 +19,10 @@
 #include "audio.h"
 #include "version.h"
 
+LEADER_EXTERNS();
+
 enum preonic_layers {
   _QWERTY,
-  _WORKMAN,
-  _NUMPAD,
   _EMPTY,
   _LOWER,
   _RAISE,
@@ -32,33 +32,8 @@ enum preonic_layers {
 enum preonic_keycodes {
   LOWER = SAFE_RANGE,
   RAISE,
-  QWERTY,
-  WORKMAN,     // Switch to workman layout
-  NUMPAD,      // Switch to numpad
-  KC_VRSN,     // Send version string
-  LF_HOME,     // Send string "~/"
-  EN_MODE,     // Cycle encoder mode
 };
 
-// Layers for different function
-enum encoder_function_t {
-    ENCODER_FUNCTION_BASE = 0,    // History scrub
-    ENCODER_FUNCTION_LOWER,       // Volume knob
-    ENCODER_FUNCTION_RAISE,       // Word scroll
-    ENCODER_FUNCTION_TEST,        // Test something new
-};
-
-// Encoder modes are selected by pressing on the encoder, each mode should have a distinctive sound.
-enum encoder_mode_t {
-  ENCODER_MODE_PRIMARY=0,
-  ENCODER_MODE_SECONDARY,
-  ENCODER_MODE_TERMINATOR,
-};
-
-// Store the mode states for different functions
-enum encoder_mode_t enc_modes[4] = {ENCODER_MODE_PRIMARY, ENCODER_MODE_PRIMARY, ENCODER_MODE_PRIMARY, ENCODER_MODE_PRIMARY};
-enum encoder_function_t enc_selected_function= ENCODER_FUNCTION_RAISE;
-uint16_t enc_held_key = XXXXXXX;
 
 #define MT_SHFT MT(MOD_RSFT, KC_ENT)   // TAP ENTER, HOLD SHIFT
 #define MT_CTRL MT(MOD_RCTL, KC_QUOT)  // TAP QUOTE, HODL LCTL
@@ -69,128 +44,123 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT_CTRL, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   MT_SHFT, \
-        EN_MODE, KC_LGUI, KC_LALT, KC_ESC,  LOWER,      KC_SPC,        RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
-        ),
-    [_WORKMAN] = LAYOUT_preonic_1x2uC( \
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  \
-        KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_BSPC, \
-        KC_LCTL, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, \
-        KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_UP,   MT_SHFT, \
-        EN_MODE, KC_LGUI, KC_LALT, KC_ESC,  LOWER,      KC_SPC,        RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
-        ),
-    [_NUMPAD] = LAYOUT_preonic_1x2uC( \
-        KC_ACL0, KC_BTN3, KC_BTN4, KC_BTN5, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
-        KC_ACL1, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
-        KC_ACL2, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, XXXXXXX, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
-        XXXXXXX, KC_WH_L, XXXXXXX, KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOWER,       XXXXXXX,      RAISE,   KC_P0,   KC_P0,   KC_PDOT, KC_PENT  \
-        ),
+        KC_LEAD, KC_LGUI, KC_LALT, KC_ESC,  LOWER,      KC_SPC,        RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
+    ),
     [_EMPTY] = LAYOUT_preonic_1x2uC( \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOWER,       XXXXXXX,      RAISE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  \
-        ),
+    ),
     [_LOWER] = LAYOUT_preonic_1x2uC( \
         KC_TILD, _______, _______, KC_STOP, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_LPRN, KC_RPRN, KC_VOLD, KC_VOLU, \
         _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_P7,   KC_P8,   KC_P9,   KC_LBRC, KC_RBRC, KC_MPLY, KC_DEL,  \
         KC_RCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_P4,   KC_P5,   KC_P6,   KC_LCBR, KC_RCBR, _______, KC_BSLS, \
         KC_RSFT, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_MPRV, KC_MNXT, KC_PGUP, KC_EXEC, \
-        EN_MODE,  KC_RGUI, KC_RALT, KC_ESC,  _______,     KC_P0,        _______, LF_HOME, KC_HOME, KC_PGDN, KC_END   \
-        ),
+        _______, KC_RGUI, KC_RALT, KC_ESC,  _______,     KC_P0,        _______, _______, KC_HOME, KC_PGDN, KC_END   \
+    ),
     [_RAISE] = LAYOUT_preonic_1x2uC( \
         KC_GRV,  _______, _______, KC_STOP, _______, KC_F1,   KC_F2,   KC_F3,   KC_LCBR, KC_RCBR, KC_VOLD, KC_VOLU, \
         _______, _______, _______, _______, _______, KC_F4,   KC_F5,   KC_F6,   KC_LBRC, KC_RBRC, KC_MPLY, KC_DEL,  \
         KC_RCTL, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_LCBR, KC_RCBR, _______, KC_PIPE, \
         KC_RSFT, _______, _______, _______, _______, KC_F10,  KC_F11,  KC_F12,  KC_MPRV, KC_MNXT, KC_PGUP, KC_EXEC, \
-        EN_MODE, KC_RGUI, KC_RALT, KC_RGUI, _______,     _______,      _______, _______, KC_HOME, KC_PGDN, KC_END   \
+        _______, KC_RGUI, KC_RALT, KC_RGUI, _______,     _______,      _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
     [_ADJUST] = LAYOUT_preonic_1x2uC( \
-        DM_REC1, DM_REC2, DM_RSTP, _______, _______, _______, _______, _______, AU_ON,   AU_OFF,  _______, QWERTY,  \
-        KC_VRSN, RESET,   DEBUG,   _______, _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, WORKMAN, \
-        KC_CAPS, EEP_RST, MU_MOD,  _______, _______, _______, _______, _______, MU_ON,   MU_OFF,  _______, NUMPAD,  \
-        KC_INS,  CK_RST,  _______, _______, _______, _______, _______, _______, MI_ON,   MI_OFF,  CK_UP,   _______, \
-        _______, _______, _______, _______, _______,     _______,      _______, _______, CK_TOGG, CK_DOWN, _______  \
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+        _______, RESET,   DEBUG,   _______, _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, \
+        _______, EEP_RST, MU_MOD,  _______, _______, _______, _______, _______, MU_ON,   MU_OFF,  _______, _______, \
+        _______, CK_RST,  _______, _______, _______, _______, _______, _______, MI_ON,   MI_OFF,  _______, _______, \
+        _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______  \
     )
 };
 
-float end_macro_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
-float start_macro_song[][2] = SONG(PLOVER_SOUND);
-float major_sound[][2] = SONG(MAJOR_SOUND);
-float chromatic_sound[][2] = SONG(CHROMATIC_SOUND);
+void print_manual(void) {
+    SEND_STRING("quick help for " QMK_KEYBOARD ":" QMK_KEYMAP " @ " QMK_VERSION "\n");
+    SEND_STRING("\nLeader key: (L=Code):\n");
+    SEND_STRING("L + h -> Print quickhelp\n");
+    SEND_STRING("L + p -> PrintScreen\n");
+    SEND_STRING("L + c -> CapsLock\n");
+    SEND_STRING("L + i -> Insert\n");
+    SEND_STRING("L + LAlt + (0-8) -> Ctrl + Alt + F#\n");
+    SEND_STRING("L + Any Number/+/- -> F#\n");
+    SEND_STRING("L + LAlt + BSPC -> Ctrl + Alt + Del\n");
+    SEND_STRING("L + c -> Caps\n");
+    SEND_STRING("L + v -> Print version/build info\n");
 
+    SEND_STRING("\nMod Tap: (Tap : Hold)\n");
+    SEND_STRING("Enter : RShift\n");
+    SEND_STRING("Quote(') : RCtrl\n");
 
-void dynamic_macro_record_end_user(int8_t direction) {
-  if (direction != 0) {
-    PLAY_SONG(end_macro_song);
+    SEND_STRING("\nAdjust Layer: Prefix with (Lower + Raise)\n");
+    SEND_STRING("Q -> RESET\n");
+    SEND_STRING("A -> RESET EEPROM\n");
+    SEND_STRING("Any Number, '+', '-' -> F#\n");
+
+    SEND_STRING("\nMouse Keys: Prefix with Lower\n");
+    SEND_STRING("W,A,S,D -> U,D,L,R\n");
+    SEND_STRING("Q,E - Left, right click\n");
+    SEND_STRING("R,F - Up, Down wheel scroll\n");
+
+}
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_ONE_KEY(KC_C) tap_code(KC_CAPS);
+    SEQ_ONE_KEY(KC_I) tap_code(KC_INSERT);
+    SEQ_ONE_KEY(KC_P) tap_code(KC_PSCR);
+
+    SEQ_ONE_KEY(KC_V) {
+        SEND_STRING("model: " QMK_KEYBOARD ", keymap:" QMK_KEYMAP ", version" QMK_VERSION ", built on:" QMK_BUILDDATE );
+    }
+
+    SEQ_ONE_KEY(KC_H) {
+        print_manual();
+    }
+
+    // Emulate Ctrl+Alt+F# for console switching
+    SEQ_TWO_KEYS(KC_LALT, KC_1) tap_code16(C(A(KC_F1)));
+    SEQ_TWO_KEYS(KC_LALT, KC_2) tap_code16(C(A(KC_F2)));
+    SEQ_TWO_KEYS(KC_LALT, KC_3) tap_code16(C(A(KC_F3)));
+    SEQ_TWO_KEYS(KC_LALT, KC_4) tap_code16(C(A(KC_F4)));
+    SEQ_TWO_KEYS(KC_LALT, KC_5) tap_code16(C(A(KC_F5)));
+    SEQ_TWO_KEYS(KC_LALT, KC_6) tap_code16(C(A(KC_F6)));
+    SEQ_TWO_KEYS(KC_LALT, KC_7) tap_code16(C(A(KC_F7)));
+    SEQ_TWO_KEYS(KC_LALT, KC_8) tap_code16(C(A(KC_F8)));
+
+    // Emulate the 3 finger salute.
+    SEQ_TWO_KEYS(KC_LALT, KC_BSPC) tap_code16(C(A(KC_DEL)));
+
+    // Function keys
+    SEQ_ONE_KEY(KC_1) tap_code(KC_F1);
+    SEQ_ONE_KEY(KC_2) tap_code(KC_F2);
+    SEQ_ONE_KEY(KC_3) tap_code(KC_F3);
+    SEQ_ONE_KEY(KC_4) tap_code(KC_F4);
+    SEQ_ONE_KEY(KC_5) tap_code(KC_F5);
+    SEQ_ONE_KEY(KC_6) tap_code(KC_F6);
+    SEQ_ONE_KEY(KC_7) tap_code(KC_F7);
+    SEQ_ONE_KEY(KC_8) tap_code(KC_F8);
+    SEQ_ONE_KEY(KC_9) tap_code(KC_F9);
+    SEQ_ONE_KEY(KC_0) tap_code(KC_F10);
+    SEQ_ONE_KEY(KC_MINS) tap_code(KC_F11);
+    SEQ_ONE_KEY(KC_EQL) tap_code(KC_F12);
   }
 }
-
-void encoder_hold_key(uint16_t keycode) {
-    if (keycode == enc_held_key) {
-        if(keycode != XXXXXXX) {
-            unregister_code(keycode);
-        }
-        enc_held_key = XXXXXXX;
-    }
-    else{
-        if(enc_held_key != XXXXXXX) {
-            unregister_code(enc_held_key);
-        }
-        register_code(keycode);
-        enc_held_key = keycode;
-    }
-}
-
-void encoder_play_sound(enum encoder_mode_t mode) {
-  switch(mode) {
-      case ENCODER_MODE_PRIMARY:
-        PLAY_SONG(major_sound);
-        break;
-      case ENCODER_MODE_SECONDARY:
-        PLAY_SONG(chromatic_sound);
-        break;
-      case ENCODER_MODE_TERMINATOR:
-      default:
-        break;
-  }
-}
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
   switch (keycode) {
-  case QWERTY:
-    if (record->event.pressed) {
-      set_single_persistent_default_layer(_QWERTY);
-      enc_selected_function = ENCODER_FUNCTION_BASE;
-    }
-    return false;
-
-  case WORKMAN:
-    if (record->event.pressed) {
-      set_single_persistent_default_layer(_WORKMAN);
-    }
-    return false;
-
-  case NUMPAD:
-    if (record->event.pressed) {
-      set_single_persistent_default_layer(_NUMPAD);
-    }
-    return false;
-
   case LOWER:
     if (record->event.pressed) {
         layer_on(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        enc_selected_function = ENCODER_FUNCTION_LOWER;
     }
     else {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        enc_selected_function = ENCODER_FUNCTION_BASE;
-        encoder_hold_key(enc_held_key);
     }
     return false;
 
@@ -198,127 +168,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         layer_on(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        enc_selected_function = ENCODER_FUNCTION_RAISE;
     }
     else {
       layer_off(_RAISE);
       update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      enc_selected_function = ENCODER_FUNCTION_BASE;
-      encoder_hold_key(enc_held_key);
-    }
-    return false;
-
-  case DM_REC1:
-    PLAY_SONG(start_macro_song);
-    return false;
-
-  case DM_REC2:
-    PLAY_SONG(start_macro_song);
-    return false;
-
-  case KC_VRSN:
-    if (record->event.pressed) {
-      SEND_STRING(QMK_KEYBOARD ":" QMK_KEYMAP " @ " QMK_VERSION);
-    }
-    return false;
-
-  case EN_MODE:
-    if (record->event.pressed) {
-      enum encoder_mode_t current_mode = enc_modes[enc_selected_function];
-      current_mode = current_mode+1;
-      if (current_mode == ENCODER_MODE_TERMINATOR) current_mode = 0;
-      enc_modes[enc_selected_function] = current_mode;
-      encoder_play_sound(current_mode);
-      encoder_hold_key(enc_held_key);
-    }
-
-    return false;
-
-  case LF_HOME:
-    if (record->event.pressed) {
-      SEND_STRING ("~/");
     }
     return false;
   }
-
   return true;
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    enum encoder_mode_t selected_mode = enc_modes[enc_selected_function];
 
-    if (enc_selected_function == ENCODER_FUNCTION_BASE) {
-       switch(selected_mode) {
-            case ENCODER_MODE_PRIMARY:     // Scroll word
-                if (clockwise) {
-                    tap_code16(C(KC_RIGHT));
-                    } else {
-                    tap_code16(C(KC_LEFT));
-                }
-                break;
-
-            case ENCODER_MODE_SECONDARY:   // Volume scroll
-                if (clockwise) {
-                    tap_code(KC_VOLU);
-                    } else {
-                    tap_code(KC_VOLD);
-                }
-                break;
-
-            case ENCODER_MODE_TERMINATOR:
-            default:
-                break;
-        }
-        return;
-
-    }
-
-    if(enc_selected_function == ENCODER_FUNCTION_RAISE) {
-        switch(selected_mode) {
-            case ENCODER_MODE_PRIMARY:
-                if (clockwise) {
-                    tap_code16(C(KC_Y));
-                } else {
-                    tap_code16(C(KC_Z));
-                }
-                break;
-
-            case ENCODER_MODE_SECONDARY:
-                if(enc_held_key != KC_LALT) encoder_hold_key(KC_LALT);
-                if (clockwise) {
-                    tap_code(KC_TAB);
-                } else{
-                    tap_code16(S(KC_TAB));
-                }
-                break;
-            case ENCODER_MODE_TERMINATOR:
-            default:
-                break;
-        }
-        return;
-     }
-
-    if(enc_selected_function == ENCODER_FUNCTION_LOWER) {
-        switch(selected_mode) {
-            case ENCODER_MODE_PRIMARY:
-                if(clockwise) {
-                    tap_code16(C(A(KC_RIGHT)));
-                }
-                else{
-                    tap_code16(C(A(KC_LEFT)));
-                }
-            case ENCODER_MODE_SECONDARY:
-                if(enc_held_key != KC_LCTL) encoder_hold_key(KC_LCTL);
-                if (clockwise) {
-                    tap_code(KC_TAB);
-                } else {
-                    tap_code16(S(KC_TAB));
-                }
-                break;
-            case ENCODER_MODE_TERMINATOR:
-            default:
-                break;
-        }
-        return;
-    }
-}
